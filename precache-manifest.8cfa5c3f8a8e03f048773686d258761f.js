8803d3d53eeabe9339469ff0302436073ca29e98
Finn Pedersen <finnpedersenfrance@gmail.com>
2020-06-08 18:14:41 +0200

    Updates

---
+++ precache-manifest.8cfa5c3f8a8e03f048773686d258761f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/taskmanager-elm/favicon.ico"
+  },
+  {
+    "revision": "97af939cf4fc33c0baf4ce1eff59680d",
+    "url": "/taskmanager-elm/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/taskmanager-elm/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/taskmanager-elm/manifest.json"
+  },
+  {
+    "revision": "50e29d31522199bfe5f8",
+    "url": "/taskmanager-elm/static/css/main.1b3244b8.chunk.css"
+  },
+  {
+    "revision": "50e29d31522199bfe5f8",
+    "url": "/taskmanager-elm/static/js/main.50e29d31.chunk.js"
+  },
+  {
+    "revision": "8e20190d8efb772864b9",
+    "url": "/taskmanager-elm/static/js/runtime~main.8e20190d.js"
+  },
+  {
+    "revision": "b9063112a1dd4b93007d",
+    "url": "/taskmanager-elm/static/js/vendors~main.b9063112.chunk.js"
+  }
+]);
